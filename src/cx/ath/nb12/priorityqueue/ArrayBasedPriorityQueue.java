c391dfe7e4f26bc5cc854dc8438f3d3932b798b9
Jordan Dinwiddy <jordan.dinwiddy@gmail.com>
2013-03-24 14:19:45 -0700

    Adding non recursive heapup and heapdown methods

--- src/cx/ath/nb12/priorityqueue/ArrayBasedPriorityQueue.java
+++ src/cx/ath/nb12/priorityqueue/ArrayBasedPriorityQueue.java
@@ -51,8 +51,8 @@ public class ArrayBasedPriorityQueue<T> implements Queue<T> {
 
 		// Add the item to the end of the tree
 		// then bubble it up until the heap condition (left child,right child < parent) holds true
-		data.add(newEntry);			// Add item to the end
-		heapUp(data.size() - 1);	// Move it up the heap
+		data.add(newEntry);					// Add item to the end
+		heapUpNonRecurse(data.size() - 1);	// Move it up the heap
 	}
 
 	@Override
@@ -73,7 +73,7 @@ public class ArrayBasedPriorityQueue<T> implements Queue<T> {
 		// node of the tree and then sink it down the heap
 		if(data.size() > 1) {
 			data.set(0, data.get(data.size() - 1));
-			heapDown(0);
+			heapDownNonRecurse(0);
 		}
 
 		// Since this node now exists elsewhere in the heap, we can delete it
@@ -87,7 +87,7 @@ public class ArrayBasedPriorityQueue<T> implements Queue<T> {
 	 * 
 	 * @param ix
 	 */
-	private void heapUp(int ix) {
+	private void heapUpRecurse(int ix) {
 
 		if(ix == 0) {
 			// Item is at the top
@@ -98,9 +98,39 @@ public class ArrayBasedPriorityQueue<T> implements Queue<T> {
 		QueueEntry<T> child = data.get(ix);
 		QueueEntry<T> parent = data.get(parentIx);
 
+		//This is how you would do it recursively
 		if(child.priority.nValue > parent.priority.nValue) {
 			swap(ix, parentIx);
-			heapUp(parentIx);
+			heapUpRecurse(parentIx);
+		}
+
+	}
+
+	/**
+	 * Moves the element at the specified index up the heap until the heap condition holds true.
+	 * 
+	 * @param ix
+	 */
+	private void heapUpNonRecurse(int ix) {
+
+		if(ix == 0) {
+			// Item is at the top
+			return;
+		}
+
+		int parentIx = (ix-1)/2;
+		QueueEntry<T> child = data.get(ix);
+		QueueEntry<T> parent = data.get(parentIx);
+
+		//This is how you would do it non recursively
+		while(child.priority.nValue > parent.priority.nValue) {
+			swap(ix, parentIx);
+
+			// Recalc indexes
+			ix = parentIx; 
+			parentIx =  (ix-1)/2;
+			child = data.get(ix);
+			parent = data.get(parentIx);
 		}
 	}
 
@@ -109,7 +139,7 @@ public class ArrayBasedPriorityQueue<T> implements Queue<T> {
 	 * 
 	 * @param ix
 	 */
-	private void heapDown(int ix) {
+	private void heapDownRecurse(int ix) {
 
 		QueueEntry<T> entry = data.get(ix);
 		int leftChildIx = 2 * ix + 1;
@@ -121,23 +151,68 @@ public class ArrayBasedPriorityQueue<T> implements Queue<T> {
 			return;
 		}
 
-		int largestChildIx = leftChildIx;
+		int largestChildIx = getLargerNodeIx(leftChildIx, rightChildIx);
 		QueueEntry<T> largestChildEntry = data.get(largestChildIx);
-
-		if(rightChildIx < data.size()) {
-			// If we have a right child and the right is greater than the left...
-			QueueEntry<T> rightChildEntry = data.get(rightChildIx);
-
-			if(rightChildEntry.priority.nValue > largestChildEntry.priority.nValue) {
-				largestChildIx = rightChildIx;
-				largestChildEntry = rightChildEntry;
-			}
-		}
 
 		if(entry.priority.nValue < largestChildEntry.priority.nValue) {
 			swap(ix, largestChildIx);
-			heapDown(largestChildIx);
+			heapDownRecurse(largestChildIx);
 		}
+	}
+
+	/**
+	 * Moves the element at the specified index down the heap until the heap condition holds true.
+	 * 
+	 * @param ix
+	 */
+	private void heapDownNonRecurse(int ix) {
+
+		int currentIx = ix;
+		
+		while(true) {
+			QueueEntry<T> currentEntry = data.get(currentIx);
+			int leftChildIx = 2 * currentIx + 1;
+			int rightChildIx = 2 * currentIx + 2;
+
+			if(leftChildIx >= data.size()) {
+				// This means there are no children for the node (left node always gets added first so if there's no
+				// left then there is no right)
+				break;
+			}
+
+			int largestChildIx = getLargerNodeIx(leftChildIx, rightChildIx);
+			QueueEntry<T> largestChildEntry = data.get(largestChildIx);
+
+			if(currentEntry.priority.nValue >= largestChildEntry.priority.nValue) {
+				break;
+			}
+
+			swap(currentIx, largestChildIx);
+			currentIx = largestChildIx;
+		}
+	}
+
+	/**
+	 * Returns the index of the node that has the larger priority. 
+	 * 
+	 * @param ix0 the index of the first node
+	 * @param ix1 the index of the second node
+	 * @return the index (either ix0 or ix1) of the node that has the higher data.get(ix).priority
+	 */
+	private int getLargerNodeIx(int ix0, int ix1) {
+
+		QueueEntry<T> entry0 = data.get(ix0);
+
+		if(ix1 < data.size()) {
+			// If we have a right child and the right is greater than the left...
+			QueueEntry<T> entry1 = data.get(ix1);
+
+			if(entry1.priority.nValue > entry0.priority.nValue) {
+				return ix1;
+			}
+		}
+
+		return ix0;
 	}
 
 	private void swap(int ixA, int ixB) {
