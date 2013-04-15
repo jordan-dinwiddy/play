43ec56dd5886754f3682419824d14712f3ffefb3
Jordan Dinwiddy <jordan.dinwiddy@gmail.com>
2013-04-14 17:19:49 -0700

    Refactor

--- src/cx/ath/nb12/trees/Driver.java
+++ src/cx/ath/nb12/trees/Driver.java
@@ -2,8 +2,8 @@ package cx.ath.nb12.trees;
 
 public class Driver {
 
-	public static void main( String argg[] )
-	{
+	public static void main(String args[]) {
+		
 		Driver d = new Driver();
 		d.doBstLookupExample();
 		d.doMinValueExample();
@@ -21,64 +21,60 @@ public class Driver {
 		BSTUtil.printTreeSingleQueue(root);
 	}
 
-	private void doPreOrderExample()
-	{
+	private void doPreOrderExample() {
+		
 		Node root = BSTUtil.buildExampleBST();
 		
-		System.out.print( "Pre-Order (recursive): " );
-		BSTUtil.TraversePreOrder( root );
+		System.out.print("Pre-Order (recursive): ");
+		BSTUtil.TraversePreOrder(root);
 		System.out.println();
 		
-		System.out.print( "Pre-Order (no recurse): " );
-		BSTUtil.TraversePreOrderNoRecurse( root );
+		System.out.print("Pre-Order (no recurse): ");
+		BSTUtil.TraversePreOrderNoRecurse(root);
 		System.out.println();
-		
 	}
 	
-	private void doTraverseExample()
-	{
+	private void doTraverseExample() {
+		
 		Node root = BSTUtil.buildExampleBST();
 		
-		System.out.print( "Pre-order: " );
-		BSTUtil.TraversePreOrder( root );
+		System.out.print("Pre-order: ");
+		BSTUtil.TraversePreOrder(root);
 		System.out.println();
 		
-		System.out.print( "In-order: " );
-		BSTUtil.TraverseInOrder( root );
+		System.out.print("In-order: ");
+		BSTUtil.TraverseInOrder(root);
 		System.out.println();
 		
-		System.out.print( "Post-order: " );
-		BSTUtil.TraversePostOrder( root );
+		System.out.print("Post-order: ");
+		BSTUtil.TraversePostOrder(root);
 		System.out.println();
-		
 	}
 	
-	private void doBstLookupExample()
-	{
+	private void doBstLookupExample() {
+		
 		Node root = BSTUtil.buildExampleBST();
 		
-		verboseLookup( root, 8 );
-		verboseLookup( root, 12 );
-		verboseLookup( root, 2 );
-		verboseLookup( root, 3 );
+		verboseLookup(root, 8);
+		verboseLookup(root, 12);
+		verboseLookup(root, 2);
+		verboseLookup(root, 3);
 	}
 	
-	private void doMinValueExample()
-	{
-		try 
-		{
+	private void doMinValueExample() {
+		try  {
 			Node root = BSTUtil.buildExampleBST();
-			int minVal = BSTUtil.min( root );
+			int minVal = BSTUtil.min(root);
 			
-			System.out.println( "Smallest value in BST: " + minVal );
+			System.out.println("Smallest value in BST: " + minVal);
 		}
-		catch( Exception e ) { System.out.println( e); }
+		catch(Exception e) { System.out.println(e); }
 	}
 	
-	private void verboseLookup( Node root, int val )
-	{
-		String result = ( BSTUtil.lookup( root, val ) == null ) ? "Not Found" : "Found";
+	private void verboseLookup(Node root, int val) {
 		
-		System.out.printf( "Looking up %d in tree at %s: %s\n", val, root, result );
+		String result = (BSTUtil.lookup(root, val) == null) ? "Not Found" : "Found";
+		
+		System.out.printf("Looking up %d in tree at %s: %s\n", val, root, result);
 	}
 }
